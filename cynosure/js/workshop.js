fbb7b5b61aa02f90a2f4f8be03a7b18c3a2feb70
vijaykrishnaa <vk596466@gmail.com>
2020-08-05 20:52:47 +0530

    cynosure added

---
+++ cynosure/js/workshop.js
@@ -0,0 +1,10 @@
+.card:hover{
+  -webkit-box-shadow:-1px 9px 40px 12px rgba(255, 255, 255, 0.1);
+  -moz-box-shadow:-1px 9px 40px -12px rgba(255, 255, 255, 0.1);
+  -box-box-shadow:-1px 9px 40px -12px rgba(255, 255, 255, 0.1);
+}
+#pk:hover{
+  -webkit-box-shadow:-1px 9px 40px 12px rgba(6, 7, 16, 0.4);
+  -moz-box-shadow:-1px 9px 40px -12px rgba(6, 7, 16, 0.4);
+  -box-box-shadow:-1px 9px 40px -12px rgba(6, 7, 16, 0.4);
+}
---
+++ cynosure/transition/style.js
@@ -0,0 +1,7 @@
+document.write(window.innerWidth);
+if(window.innerWidth <800){
+    document.write("hello small");
+}
+else {
+    document.write("hello");
+}
